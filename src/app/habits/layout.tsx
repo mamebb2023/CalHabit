7d838bef012027919209e6a985a13d5c9ab51055
Mohammednur <mamebb2023@gmail.com>
2024-11-10 18:42:56 +0300

    Remove unused components and types; update package name and dependencies

--- src/app/habits/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-color-tertiary">{children}</div>;
-};
-
-export default Layout;
