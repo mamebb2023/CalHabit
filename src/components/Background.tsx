f8ea3958fce3f64a9bf3964ebb1eafb764acebcd
Mohammednur <mamebb2023@gmail.com>
2024-11-24 12:24:49 +0300

    Update Background component to change gradient color from secondary to tertiary

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Background = ({ children }: { children: React.ReactNode }) => {
   return (
     // remove - from aurora- to change background
-    <div className="aurora- bg-gradient-to-tr from-color-secondary to-white">
+    <div className="aurora- bg-gradient-to-tr from-color-tertiary to-white">
       {children}
     </div>
   );
